3182aa0c7be2d738449b20d7eab99c14eb38a316
Trevor Yearwood <Trev_Yearwood@hotmail.com>
2022-07-02 15:39:17 +0100

    template added

---
+++ YearwoodSolutions.Application.Web/Pages/main.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YearwoodSolutions.Application.Web.Pages
+{
+    public class MainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
